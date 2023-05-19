d64348cb96eb4f688940850aa8aaa0b09644d9be
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-05-19 17:52:16 +0100

    Make sure password fields match

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const inputs = document.querySelectorAll('input');
 
 const submitButton = document.querySelector('input[type="submit"]');
 const passwordConfirm = document.querySelector('#passwordConfirm');
+const password = document.querySelector('#password');
+
+function passwordChecker() { //checks if passwords match, will style differently if they do not.
+    if (password.value != passwordConfirm.value) passwordConfirm.classList.add('invalid');
+    else passwordConfirm.classList.remove('invalid');
+}
 
 submitButton.addEventListener('click', () => {
     inputs.forEach(input => {
@@ -12,4 +18,5 @@ submitButton.addEventListener('click', () => {
     });
 });
 
-// passwordConfirm.addEventListener('blur', ())
+passwordConfirm.addEventListener('blur', () => passwordChecker());
+password.addEventListener('blur', () => passwordChecker());
