33a85d0d1a8870f47ac1e5c96ee529399ce8b5dc
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-05-19 17:30:05 +0100

    Remove e from event listeners (unneeded)

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const inputs = document.querySelectorAll('input');
-const submitButton = document.querySelector('input[type="submit"]');
 
-submitButton.addEventListener('click', (e) => {
+const submitButton = document.querySelector('input[type="submit"]');
+const passwordConfirm = document.querySelector('#passwordConfirm');
+
+submitButton.addEventListener('click', () => {
     inputs.forEach(input => {
-        input.addEventListener('invalid', (e) => {
+        input.addEventListener('invalid', () => {
             input.classList.add('invalid');
             console.log(input);
         })
     });
 });
+
+// passwordConfirm.addEventListener('blur', ())
