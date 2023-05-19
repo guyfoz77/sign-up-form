9d2798e4bf13208921913e9606debf40a4482f79
Guy <125807123+guyfoz77@users.noreply.github.com>
2023-05-19 18:35:35 +0100

    Disable submit button if passwords dont match

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const passwordConfirm = document.querySelector('#passwordConfirm');
 const password = document.querySelector('#password');
 
 function passwordChecker() { //checks if passwords match, will style differently if they do not.
-    if (password.value != passwordConfirm.value) passwordConfirm.classList.add('invalid');
-    else passwordConfirm.classList.remove('invalid');
+    if (password.value != passwordConfirm.value) {
+        passwordConfirm.classList.add('invalid');
+        submitButton.disabled = true;
+    } else {
+        passwordConfirm.classList.remove('invalid');
+        submitButton.disabled = false;
+    }
 }
 
 submitButton.addEventListener('click', () => {
     inputs.forEach(input => {
         input.addEventListener('invalid', () => {
             input.classList.add('invalid');
-            console.log(input);
         })
     });
 });
